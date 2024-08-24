bb8ad44098789f660de7c06b29151de03808016f
Georgi Markov <gogata1905@gmail.com>
2024-08-24 19:48:20 +0300

    update nav components

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,17 +7,17 @@ import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: '',
-    runGuardsAndResolvers: 'always',
-    canActivate: [authGuard],
-    children: [
-      { path: 'members', component: MemberListComponent },
-      { path: 'members/:id', component: MemberDetailComponent },
-      { path: 'lists', component: ListsComponent },
-      { path: 'messages', component: MessagesComponent },
-    ],
-  },
-  { path: '**', component: HomeComponent, pathMatch: 'full' },
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component: MemberListComponent},
+            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'lists', component: ListsComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
+    {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
